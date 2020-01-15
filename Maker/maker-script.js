f1dd2fe2fec4e3edf3b63cdcac31f764cdb2c3a4
fMuchka <37208551+fMuchka@users.noreply.github.com>
2020-01-15 23:00:10 +0100

    Version 01

--- Maker/maker-script.js
+++ Maker/maker-script.js
@@ -1,244 +1,224 @@
+
 var quiz = {
-    themes: undefined
+    theme_ammount:  null,
+    question_ammount:   null,
+    
+    themes: []
 }
 
-var themes_html = {
-    inputs:         document.getElementsByClassName('theme-inputs'),
-    navigations:    undefined,
-    current_theme:  undefined
-}
+var theme_map = {};
+var question_map = {};
 
-var selected_question = null;
-
-function SwapClasses(element, classRemove, classAdd) {
-    new Promise ((resolve, reject) => {
-        setTimeout( () => {
-            if (classRemove) {
-                if (!Array.isArray(element)) {
-                    element.classList.remove(classRemove);    
-                }
-                else{
-                    for (let i = 0; i < element.length; i++) {
-                        element[i].classList.remove(classRemove);
-                    }
-                }   
-            }
-            resolve();
-        }, 10);
-    }).then( () =>{
-        setTimeout( () =>{
-            if (classAdd) {
-                if (!Array.isArray(element)) {
-                    element.classList.add(classAdd);    
-                }
-                else{
-                    for (let i = 0; i < element.length; i++) {
-                        element[i].classList.add(classAdd);
-                    }
-                }    
-            }
-        }, 10)
-    })
-}
-
-function CreateThemes(themes){
-    quiz.themes = [];
-
-    let nav_wrapper = document.getElementById('theme-navigation-wrapper');
-
-    for (let i = 0; i < themes.length; i++) {
-        if (themes[i].value !== '') {
-            quiz.themes.push(themes[i].value);
-
-            let nav = document.createElement('button');
-            nav.classList.add('theme-navigation');
-            nav.onclick = ShowTheme;
-            nav.setAttribute('data-theme-index', quiz.themes.length - 1);
-            nav.innerText = themes[i].value;
-
-            nav_wrapper.appendChild(nav);   
-        }
-    }
-}
-
-function ShowTheme() {
-    let button = event.target;
-
-    if (themes_html.current_theme !== null) {
-        themes_html.current_theme.classList.remove('selected');
-        themes_html.current_theme = null;
-    }
-    button.classList.add('selected');
-
-    themes_html.current_theme = event.target;
-}
-
-function ConfirmThemes() {
-    let themes = document.getElementsByClassName('theme-input');
-    let confirm = document.getElementById('confirm-themes');
-    let navigation = document.getElementById('navigation-wrapper');
-    let main_area = document.getElementById('main-area');
-    let buttons = {delete: document.getElementById('delete-question'),
-                   add: document.getElementById('add-question')
-                };
-
-    new Promise ( (resolve, reject) => {
-        CreateThemes(themes);
-        resolve();
-    }).then( () => {
-        setTimeout( () => {
-            for (let i = themes.length - 1; i != -1; i--) {
-                SwapClasses(themes[i].parentNode, 'fade-in', 'fade-out');
-            }   
-            SwapClasses(confirm, 'fade-in', 'fade-out');
-            SwapClasses([buttons.delete, buttons.add], 'invisible', false);
-            main_area.classList.add('work');
-        }, 50)
-    }).then( () => {
+function ShowCreateMenu() {
+    new Promise((resolve, reject) => {
         setTimeout(() => {
-            for (let i = themes.length - 1; i != -1; i--) {
-                themes[i].parentNode.remove();
-                SwapClasses([navigation, buttons.delete, buttons.add], 'fade-out', 'fade-in');
-            }
-            confirm.remove();
-        }, 500);
-    }).then( () => {
-        let first_theme = document.getElementsByClassName('theme-navigation')[0];
-        first_theme.classList.add('selected');
-        themes_html.current_theme = first_theme;
+            this.page_elements.menus.main.style.opacity = 0;
+            this.page_elements.menus.create.style.opacity = 1;
+            
+        }, 150);
+        resolve();
+    }).then(() => {
+        setTimeout(() => {
+            this.page_elements.menus.main.classList.toggle('hidden');
+            this.page_elements.menus.create.classList.toggle('hidden');       
+        }, 200);
+    }).then(() => {
+        setTimeout(() => {
+            this.page_elements.menus.main.classList.toggle('invisible');
+        }, 250)
     });
 }
 
-function ShowNextThemeInput(index) {
-    
+function HideCreateMenu() {
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            this.page_elements.menus.create.style.opacity = 0;
+        }, 150);
+        resolve();
+    }).then(() => {
+        setTimeout(() => {
+            this.page_elements.menus.create.classList.toggle('hidden');
+        }, 200);
+    }).then(() => {
+        setTimeout(() => {
+            this.page_elements.menus.create.classList.toggle('invisible');
+        }, 250);
+    });
 }
 
-function CreateThemeInput() {
-    let code;
+function ShowQuizMenu() {
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            this.page_elements.menus.quiz.style.opacity = 1;
+        }, 150);
+        resolve();
+    }).then(() => {
+        setTimeout(() => {
+            this.page_elements.menus.quiz.classList.toggle('hidden');
+        }, 200);
+    });
+}
 
-    if (event === undefined) {
-         code = 13;
+function HideQuizMenu() {
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            this.page_elements.menus.quiz.style.opacity = 0;
+        }, 150);
+        resolve();
+    }).then(() => {
+        setTimeout(() => {
+            this.page_elements.menus.quiz.classList.toggle('hidden');
+        }, 200);
+    }).then(() => {
+        setTimeout(() => {
+            this.page_elements.menus.quiz.classList.toggle('invisible');
+        }, 250);
+    });
+}
+
+function ShowThemeInfo(theme) {
+    //  TO-DO:  SHOW THE THEME MENU FROM THEME-MENUS-WRAPPER
+}
+
+function CheckNumberInput() {
+    let theme_ammount = this.page_elements.inputs.theme_ammount;
+    let question_ammount = this.page_elements.inputs.question_ammount;
+
+    if (theme_ammount.value != 0 && question_ammount.value != 0) {
+        this.page_elements.buttons.create_confirm.disabled = false;
     }
     else{
-         code = event.keyCode;
-    }
-
-    if (themes_inputs.current < themes_inputs.maximum && code === 13){
-        let wrapper = document.getElementById('main-area');
-        let confirm = document.getElementById('confirm-themes');
-
-        let input = {
-            wrapper: document.createElement('div'),
-            field: document.createElement('input'),
-            cancel: document.createElement('button')
-        };
-
-        input.wrapper.classList.add('theme-input-wrapper', 'fade-out');
-        input.wrapper.appendChild(input.field);
-        input.wrapper.appendChild(input.cancel);
-        input.field.classList.add('theme-input');
-        input.field.placeholder = "Theme #" + (themes_inputs.current + 1);
-        input.field.onkeyup = CreateThemeInput;
-        
-        input.cancel.classList.add('input-cancel');
-        input.cancel.onclick = RemoveThemeInput;
-
-        wrapper.insertBefore(input.wrapper, confirm);
-
-        themes_inputs.current++;
-
-        new Promise( (resolve, reject) => {
-            setTimeout( () => {
-
-                SwapClasses(input.wrapper, 'fade-out', 'fade-in');
-                resolve();
-            }, 50)
-        });        
-    }
-
-    if (themes_inputs.current > 0) {
-        let confirm = document.getElementById('confirm-themes');
-
-        new Promise( (resolve, reject) => {
-            setTimeout( () => {
-                confirm.classList.remove('invisible');
-                resolve();
-            }, 100)
-        }).then(() => {
-
-            SwapClasses(confirm, 'fade-out', 'fade-in');
-        });
-    } 
-}
-
-function RemoveThemeInput() {
-    let wrapper = event.target.parentNode;
-
-    if (themes_inputs.current > 1) {
-        new Promise( (resolve, reject) => {
-            setTimeout( () => {
-
-              SwapClasses(wrapper, 'fade-in', 'fade-out');
-                resolve();
-            }, 50)
-        }).then(() => {
-            setTimeout( () => {
-                wrapper.remove();        
-            }, 500);
-        });
-
-        themes_inputs.current--;    
+        this.page_elements.buttons.create_confirm.disabled = true;
     }
 }
 
-function GetStarted(){
-    let create_themes = event.target;
-    let wrapper = document.getElementById('main-area');
+function StartCreation() {
+    let theme_ammount = this.page_elements.inputs.theme_ammount;
+    let question_ammount = this.page_elements.inputs.question_ammount;
 
-    create_themes.classList.add('fade-out');
+    this.quiz.theme_ammount = parseInt(theme_ammount.value, 10);
+    this.quiz.question_ammount = parseInt(question_ammount.value, 10);
 
-    new Promise( (resolve, reject) => {
-        setTimeout( () => {
-            resolve();
-        }, 500);
-    }).then(() => {
+    HideCreateMenu();
+    ShowQuizMenu();
 
-        create_themes.classList.add('invisible');
-        wrapper.classList.remove('intro');
-
-    }).then( new Promise((resolve, reject) => {   
-        setTimeout( () => {
-            CreateThemeInput();
-            resolve();    
-        }, 500);
-    }));     
-}
-
-function DeleteSelectedQuestion() {
-    const button = event.target;
-
-    if (!button.classList.contains('disabled')) {
-        selected_question.remove();
-
-        console.log('delete');
-        
-    }
-}
-
-function CreateQuestion() {
-    const button = event.target;
-
-    if (!button.classList.contains('disabled')) {
-        NewQuestion();
-    }
-}
-
-function NewQuestion(){
-    console.log('new');
+    FillQuizMenu();
     
+}
+
+
+function FillQuizMenu() {
+    AddThemes(this.page_elements.wrappers.themes);
+
+    CreateThemeMenus(this.page_elements.wrappers.theme_menus);
+
+    // TO-DO:   ADD 'CREATE JSON' BUTTON
+}
+
+function AddThemes(wrapper) {
+    for (let i = 0; i < quiz.theme_ammount; i++) {
+        let theme = document.createElement('button');
+        theme.innerText = 'Unfinished Theme';
+        theme.classList.add('theme-button');
+        theme.dataset.index = i;
+
+        theme.addEventListener('click', () => {
+            HideQuizMenu();
+            ShowThemeInfo(theme);
+        });
+
+        theme_map[i] = {};    
+        theme_map[i]['button'] = theme;
+
+        wrapper.appendChild(theme);
+    }
+}
+
+function AddQuestions(wrapper, theme_index) {
+    question_map[theme_index] = {};
+
+    for (let i = 0; i < quiz.question_ammount; i++) {
+        let question = document.createElement('div');
+        question.classList.add('question-wrapper');
+
+        if (i > 0) {
+            question.classList.add('invisible');
+            question.style.opacity = 0;
+        }
+
+        wrapper.appendChild(question);
+
+        let question_text = document.createElement('textarea');
+        question_text.placeholder = 'Enter Question';
+        question_text.classList.add('question');
+        question.appendChild(question_text);
+
+        let question_media_path = document.createElement('input');
+        question_media_path.placeholder = 'Enter Media Path';
+        question_media_path.classList.add('media-path-input');
+        question.appendChild(question_media_path);
+
+        let question_answer = document.createElement('textarea');
+        question_answer.placeholder = 'Enter Answer';
+        question_answer.classList.add('answer');
+        question.appendChild(question_answer);
+
+        let ready = document.createElement('button');
+        ready.innerText = 'Ready';
+        ready.classList.add('ready-button');
+        question.appendChild(ready);
+
+        question_map[theme_index][i] = {};
+        question_map[theme_index][i]['text'] =  question_text;
+        question_map[theme_index][i]['media'] =  question_media_path;
+        question_map[theme_index][i]['answer'] =  question_answer;
+    }
+}
+
+function CreateTitleField(wrapper) {
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Unfinished Theme';
+    input.classList.add('theme-title-input');
+
+    wrapper.appendChild(input);
+
+    this.page_elements.inputs.theme_titles.push(input);
+}
+
+function CreateThemeMenus(wrapper) {
+    for (let i = 0; i < quiz.theme_ammount; i++) {
+        let menu = document.createElement('div');
+        menu.classList.add('theme-menu', 'menu', 'invisible');
+        menu.style.opacity = 0;
+        wrapper.appendChild(menu);
+
+        theme_map[i]['menu'] = menu;
+
+        CreateTitleField(menu);
+
+        AddQuestions(menu, i);
+    }   
 }
 
 window.onload = () => {
-    
- 
+    this.page_elements = {
+        menus: {
+            main:   document.getElementById('main-menu'),
+            create: document.getElementById('create-menu'),
+            quiz:   document.getElementById('quiz-menu')
+        },
+        buttons: {
+            create_confirm: document.getElementById('create-confirm')
+        },
+        inputs: {
+            theme_ammount:      document.getElementById('theme-ammount'),
+            question_ammount:   document.getElementById('question-ammount'),
+            theme_titles:       []
+        },
+        wrappers: {
+            themes:         document.getElementById('themes-wrapper'),
+            theme_menus:    document.getElementById('theme-menus-wrapper')
+        }
+    }
 }
